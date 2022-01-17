09285b4aac4d115ed43bd9ca9a9f57579ec93551
Tarun Singh <67340592+tarungit01@users.noreply.github.com>
2022-01-17 22:35:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './Todolist/Code/todo';
+
+function App() {
+  return (
+    <>
+      <Todo />
+    </>
+
+  );
+};
+
+export default App;
